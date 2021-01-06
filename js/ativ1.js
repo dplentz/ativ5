db9061fb237f0b8e7f3a6b0bd0cd72c5f3dfe402
Maria Eduarda <dudaplentz123@gmail.com>
2021-01-06 11:38:32 -0300

    Base e opções feitas

---
+++ js/ativ1.js
@@ -0,0 +1,6 @@
+function foto(){
+    if(animais.value == "cachorro"){
+        imgfim.src = "imagens/cachorro.jpg";
+    }
+    
+}
---
+++ js/ativ2.js
@@ -0,0 +1,7 @@
+function foto(){
+    if(musica.value == "Pop"){
+        imagem.src = "imagens/pop.jpg";
+        audios.src = "audio/pop.mp3";
+        audios.play();
+    }
+}
